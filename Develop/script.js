df0c0469265917f4b30839ad06eaa01f64d21300
Christopher-VA <cvonaltenstadt@gmail.com>
2023-10-30 19:07:14 +0800

    Storing characters if they are wanted for password

--- Develop/script.js
+++ Develop/script.js
@@ -2,37 +2,95 @@
 var generateBtn = document.querySelector("#generate");
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var specialCharacter = ['!', '@', '#', '$', '%', '^', '&', '*', '?']
+var specialCharacters = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.',
+];
+var upperCasedCharacters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z',
+];
 
-function selector() {
-  var index = Math.floor(Math.random() * alphabet.length);
-  var randAlphabet = alphabet[index];
-  var options = Math.floor(Math.random() * specialCharacter.length)
-  var randSCharacters = specialCharacter[options];
-  var randNumber = Math.floor(Math.random() * numbers.length);
 
+function generatePassword() {
+  // var index = Math.floor(Math.random() * alphabet.length);
+  // var randAlphabet = alphabet[index];
+  // var options = Math.floor(Math.random() * specialCharacter.length)
+  // var randSCharacters = specialCharacter[options];
+  // var randNumber = Math.floor(Math.random() * numbers.length);
+
+  var characters = window.confirm("Do you want the password to have Lowercased Characters")
   var numeric = window.confirm("Do you want the password to have Numbers?");
-  var Uppercase = window.confirm("Do you want the password to have Uppercase?");
+  var Uppercase = window.confirm("Do you want the password to have Uppercased Characters?");
   var special = window.confirm("Do you want the password to have Special Characters");
 
-
-  if (numeric = true) {
-  }
-}
-
-
-
-
-
-
-// Write password to the #password input
-function writePassword() {
-  // var password = generatePassword();
-  // var passwordText = document.querySelector("#password");
-
-  // passwordText.value = password;
-
   var minAmount = window.prompt("Minimum passcode?")
+  var combinedCharacters = [];
+
+  if (numeric) {
+    // combinedCharacters.concat(numbers);
+    combinedCharacters = combinedCharacters + numbers;
+  }
+
+  if (Uppercase) {
+    // combinedCharacters.concat(upperCasedCharacters);
+    combinedCharacters = combinedCharacters + upperCasedCharacters;
+  }
+
+  if (special) {
+    // combinedCharacters.concat(specialCharacters);
+    combinedCharacters = combinedCharacters + specialCharacters;
+  }
+
+  if (characters) {
+    // combinedCharacters.concat(alphabet);
+    combinedCharacters = combinedCharacters + alphabet;
+  }
 
   if (minAmount > 126) {
     window.alert("Too many characters!");
@@ -45,9 +103,21 @@ function writePassword() {
     return
   }
   else {
-    selector();
+    // writePassword();
+    window.alert(combinedCharacters);
   }
+
+}
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+generatePassword();
